46717f5b1f6a16b2eaee271473824d11f4cafe75
Lanik2009 <79287838+Lanik2009@users.noreply.github.com>
2024-05-08 21:33:49 -0700

    Create new.js

---
+++ new.js
@@ -0,0 +1,8 @@
+function toggleText() {
+            var text = document.getElementById("hiddenText");
+            if (text.style.display === "none") {
+                text.style.display = "block"; // Show the text
+            } else {
+                text.style.display = "none"; // Hide the text
+            }
+        }
